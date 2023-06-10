a49f4f4e8af7593ef5ef46ef66c0981c45c2129e
SAMPREETI2002 <sampreetireddy19@gmail.com>
2023-06-11 02:20:53 +0530

    Adding the orientation pages

---
+++ src/Images/ImgEnvir.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./ImgStyles.css"
+export default function ImgEnvir(props) {
+  return (
+    <div>
+      <img src={props.img}/>
+      {/* <img src={props.content}/> */}
+    </div>
+  )
+
+}
---
+++ src/Orientation/Orient.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import './OrientStyles.css'
+export default function Orient() {
+  return (
+    <div className="container">
+      <div className="card" style={{width: "25rem",height:"35rem",marginTop:"40px"}}>
+  <img src="https://media.istockphoto.com/id/1327919661/photo/world-earth-day-concept-green-energy-renewable-and-sustainable-resources-environmental-and.jpg?s=612x612&w=0&k=20&c=6PvLRMFPI65ATEs-XWPJ_UOtkmSKoONOD4h7Lohq850=" className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h3 className="card-title">Environmental Sustainability</h3>
+    <a href="/" className="btn btn-primary">Read more</a>
+  </div>
+  
+</div>
+<div className="card" style={{width: "25rem",height:"35rem",marginTop:"40px"}}>
+  <img src="https://life.futuregenerali.in/media/v1vh4v1o/how-does-insurance-help-in-economic-development.jpg" className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h3 className="card-title">Economic Development</h3>
+    <a href="/" className="btn btn-primary">Read more</a>
+  </div>
+</div>
+  <div className="card" style={{width: "25rem",height:"35rem",marginTop:"40px"}}>
+  <img src="https://www.evidencebasedmentoring.org/wp-content/uploads/2020/01/ground-group-growth-hands-461049-1024x720.jpg" className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h3 className="card-title">Social <br/>Development </h3>
+    <a href="/" className="btn btn-primary">Read more</a>
+  </div>
+</div>
+  <div className="card" style={{width: "25rem",height:"35rem",marginTop:"40px"}}>
+  <img src="https://media.istockphoto.com/id/492878594/photo/teamwork.jpg?s=612x612&w=0&k=20&c=zJ1NtL3NdsKeP_aeYkJB8yyf6-i0uD3EjFVvz7IZaJY=" className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h3 className="card-title">Partnerships for the goal</h3>
+    <a href="/" className="btn btn-primary">Read more</a>
+  </div>
+  
+</div>
+    </div>
+  )
+}
