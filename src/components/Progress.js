3691bb75d03c6260f21e63f31fedee2ef454fba9
Shreshtha Kochgaway <skochgaway112@gmail.com>
2023-06-11 02:31:57 +0530

    making changes in the Progress Bar

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,5 +1,37 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 const Progress = () => {
-  return <div></div>;
+  
+  const [users, setUsers] = useState([]);
+  const [user, setUser] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  useEffect(() => {
+    getUserData();
+  }, []);
+
+
+  const getUserData = async ()=>{
+
+   
+
+    let current = await axios.get("http://localhost:4000/users");
+    setUsers(current);
+    console.log(users);
+  }
+    
+  
+  return(
+<>
+
+</>
+  ) 
+   
+   
+
+
+ 
 };
+
 
 export default Progress;
