e617417e8a9676435d18dfe5433f5f533a971602
AKHIL3SH-KUMAND1 <96103942+AKHIL3SH-KUMAND1@users.noreply.github.com>
2023-06-11 03:36:51 +0530

    App and Index Correction

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-import './App.css'
-import Topbar from './components/Topbar';
-import 'bootstrap/dist/css/bootstrap.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import certificationpage from './certificationpage';
-function App() {
-  return (
-    <div className="App">
-      <Topbar />
-    </div>
-  );
-}
+import "./App.css";
+import Topbar from "./components/Topbar";
+import "bootstrap/dist/css/bootstrap.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 const App = () => {
   return (
-    <Router>
-      <Route path="/certificate" component={certificationpage} />
-    </Router>
+    <div className="App">
+      <Router>
+        <Topbar />
+      </Router>
+    </div>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Certificate from './Certificate';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-root.render(
-  <React.StrictMode>
-    <CertificatePage/>
-  </React.StrictMode>
-);
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+// import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+// import Certificate from './Certificate';
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React>
+//     <App />
+//   </React>
+// );
 
+ReactDOM.render(<App />, document.getElementById("root"));
